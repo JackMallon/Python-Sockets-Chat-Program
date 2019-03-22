b912f9fe8a9ef6f1f9256b9f12fa15dbba877bf1
Jack Mallon <jack.mallonk@gmail.com>
2019-03-22 23:24:22 +0000

    Final version

--- c.py
+++ c.py
@@ -2,50 +2,83 @@ import threading
 import socket
 import os
 import webbrowser
+import time
 
-HOST = '80.233.39.231'
-PORT = 20041         
+HOST = '127.0.0.1'
+PORT = 3500
+username = None
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-username = None
+def commandlist():
+    commandlist = 	(
+    "**************************************************************\n"
+    "***                      Commands                          ***\n"
+    "**************************************************************\n"
+    "***              !time - displays current time             ***\n"
+    "***     !total - displays total messages in buffer         ***\n"
+    "***             !users - displays current users            ***\n"
+    "***                  !flip - flips a coin                  ***\n"
+    "***             !qotd - print quote of the day             ***\n"
+    "***    !google - opens browser on other users machines     ***\n"
+    "***            !day - displays current day of week         ***\n"
+    "***              !date - displays current date             ***\n"
+    "***         !ping - prints pong and latency time           ***\n"
+    "***        !commands - displays current commands           ***\n"
+    "**************************************************************")
+    print commandlist
 
 # Open youtube search
 def google(data):
     print("Google function")
     googleStr = data.replace("!google","")
-    googleStr = googleStr.split(':', 1)[-1] = googleStr.strip()
+    googleStr = googleStr.split(':', 1)[-1]
+    googleStr = googleStr.strip()
     googleStr = googleStr.replace(" ","+")
+    print(googleStr)
     url = "https://www.google.com/search?q=" + googleStr
     webbrowser.open_new_tab(url)
 
-# Open youtube search
+# Ping pong
 def ping():
-    pingSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    pingSock.connect((HOST, PORT))
+    global HOST, PORT
+    pingSoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    pingSoc.connect((HOST, PORT))
+    start = time.time()
+    pingSoc.sendall(str("!ping"))
+    data = pingSoc.recv(100)
+    print(data)
+    end = time.time()
+    amount = (end - start)
+    print("Ping took " + str(amount) + " seconds")
+    pingSoc.shutdown(socket.SHUT_RDWR)
+    pingSoc.close()
 
+def processOutput(text):
+    if "!ping" in text:
+        ping()
+        return False
+    if "!commands" in text:
+        commandlist()
+        return False
+    return True
 
 def parse(data):
     # Check if functions are called
-    if "!flip" in data:
-        flip()
     if "!google" in data:
         google(data)
-    if "!ping" in data:
-        ping()
-
 
 def readInputThreaded(so):
     global username
     print("Type your username:")
     username = raw_input()
     so.sendall(str("!setUsername" + username))
-
     while 1:
         text = raw_input()
         text = username + ": " + text
-        so.sendall(str(text))
+        if processOutput(text):
+            so.sendall(str(text))
 
 t = threading.Thread(target=readInputThreaded, args = (s,))
 t.start()
--- s.py
+++ s.py
@@ -4,10 +4,12 @@ import threading, Queue
 from time import gmtime, strftime
 import time
 from time import gmtime, strftime
+import time
+import datetime
 
 # IP address goes here
-HOST = '80.233.39.231'    
-PORT = 20041
+HOST = '127.0.0.1'
+PORT = 3500
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
@@ -15,35 +17,75 @@ s.bind((HOST, PORT))
 # A list of all current connections
 currentConnections = list()
 # Log of the chat
-buffer = "Beginning of chat : "
+buffer = "Beginning of chat | "
+# Number of messages in buffer
+bufferCounter = 0
 # List of usernames
 usernames = "Users in this chat: "
+#Key value pair of users and their connection object
+userConnectionsDict = dict()
+#Kicked users
+kickedUsers = list()
 
 # The functions we've added
+def qotd():
+    from random import randint
+    randomQOTD = randint(0, 9)
+    quotes = ["'A yurt a day keeps the doctor away.' - Gav", "'Change the world by being a Yurt.' - Amy Poehler", "'Every Yurt is a fresh beginning.' - T.S Eliot",
+    "'Never regret anything that made you Yurt.' - Mark Twain", "'Everything you can Yurt is real.' - Pablo Picasso", "'Whatever you do, Yurt it well.' - Walt Disney",
+    "'Tough times never last but Yurts people do.' - Robert H. Schiuller", "'Have enough courage to Yurt and enough heart to Yurt Harder.' - Jessica N. S. Yourko",
+    "'Be so good they cant Yurt you' - Steve Martin", "'Never let your emotions over power your Yurt.' - Drake"]
+    for singleClient in currentConnections:
+        singleClient.send(str(quotes[randomQOTD]))
+
+def addToBuffer(data):
+    global buffer
+    global bufferCounter
+    bufferCounter += 1
+    buffer = buffer + data + " | "
+
+def total():
+    global bufferCounter
+    for singleClient in currentConnections:
+        singleClient.send(str(bufferCounter))
+
+def day():
+    day = ""
+    day ="Today is "+ datetime.date.today().strftime("%A")
+    for singleClient in currentConnections:
+        singleClient.send(str(day))
+
 # Coin flip
 def flip():
     from random import randint
     randomflip = randint(0, 1)
-
     if randomflip == 0:
         flip = "heads"
     elif randomflip == 1:
         flip = "tails"
-
     for singleClient in currentConnections:
         singleClient.send(str(flip))
 
-def checkData(data):
+def checkData(data, conn):
+    global kickedUsers
     if "!setUsername" in data:
+        return False
+    if "!ping" in data:
+        return False
+    if conn in kickedUsers:
         return False
     return True
 
-def addUsername(data):
+def addUsername(data, con):
     global usernames
+    global userConnectionsDict
     userData = data.replace("!setUsername","")
     usernames = usernames + userData + " "
-    for singleClient in currentConnections:
-        singleClient.send(str(userData + " has joined the chat!"))
+    userConnectionsDict[userData] = con
+    con.send(str(buffer + "\n"))
+    if "!ping" not in userData:
+        for singleClient in currentConnections:
+            singleClient.send(str(userData + " has joined the chat!"))
 
 def getUsers():
     global usernames
@@ -54,18 +96,52 @@ def sendTime():
     for singleClient in currentConnections:
         singleClient.send(strftime("%H:%M:%S", gmtime()))
 
+def kick(data):
+    global kickedUsers
+    global userConnectionsDict
+    user = data.split(':', 1)[-1]
+    user = user.replace("!kick","")
+    user = user.strip()
+    for singleClient in currentConnections:
+        singleClient.send(str(user + " has been kicked from the chat! They well no longer be able to send or recieve messages!"))
+    object = userConnectionsDict[user]
+    currentConnections.remove(object)
+    kickedUsers.append(object)
+
+def ping(con):
+    global currentConnections
+    con.send(str("pong"))
+    currentConnections.remove(conn)
+
+def date():
+    date= ""
+    date ="Todays date is "+ datetime.date.today().strftime("%d-%m-%Y")
+    for singleClient in currentConnections:
+        singleClient.send(str(date))
+
 # Parse messages to see if functions are called
 def parseInput(data, con):
-    print str(data)
     # Check if functions are called
     if "!flip" in data:
         flip()
     if "!setUsername" in data:
-        addUsername(data)
+        addUsername(data, con)
     if "!users" in data:
         getUsers()
     if "!time" in data:
         sendTime()
+    if "!ping" in data:
+        ping(con)
+    if "!kick" in data:
+        kick(data)
+    if "!qotd" in data:
+        qotd()
+    if "!total"in data:
+        total()
+    if "!day" in data:
+        day()
+    if "!date" in data:
+        date()
 
 #manages each connection
 def manageConnection(conn, addr):
@@ -77,9 +153,11 @@ def manageConnection(conn, addr):
     # Continue to listen
     while 1:
         data = conn.recv(1024)
+        if "!" not in data:
+            addToBuffer(data)
         for singleClient in currentConnections:
-            if singleClient != conn:
-                if checkData(data):
+            if singleClient != conn :
+                if checkData(data, conn):
                     singleClient.send(str(data))
         parseInput(data,conn)# Calling the parser
 
